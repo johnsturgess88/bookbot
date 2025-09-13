edc47c91d4507940f0c1daef939dd55a4ca63835
Johnsturgess88 <Johnsturgess88@gmail.com>
2025-09-13 10:11:36 +0100

    letter count

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+from stats import get_num_words 
+from stats import count_characters
+from stats import sort
 
 
 def get_book_text(filepath):
         with open(filepath) as f:
            text = f.read()
         return text
-        
-from stats import get_num_words
-
+     
 def main():
         content = get_book_text("./books/frankenstein.txt")
         num_words = get_num_words(content) 
         print (f"{num_words} words found in the document")     
+        print (count_characters(content))
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def get_num_words(text):
         return len(text.split())
+
+def count_characters(text):
+        counts = {}
+        for ch in text:
+            ch = ch.lower()
+            counts[ch] = counts.get(ch, 0) +1
+        return counts
+        
+def sort(text):
+       return counts.sort(reverse = true, key=sort)
+       
