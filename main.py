70a9fa0c11b75d5f2e03013e7da947f3c4918634
Johnsturgess88 <Johnsturgess88@gmail.com>
2025-09-13 17:03:48 +0100

    alpabetically sorted

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from stats import get_num_words 
 from stats import count_characters
-from stats import sort
+from stats import build_sorted_char_list
 
 
 def get_book_text(filepath):
@@ -11,6 +11,14 @@ def get_book_text(filepath):
 def main():
         content = get_book_text("./books/frankenstein.txt")
         num_words = get_num_words(content) 
-        print (f"{num_words} words found in the document")     
-        print (count_characters(content))
+        counts = count_characters(content)
+        sorted_chars = build_sorted_char_list(counts)
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print("----------- Word Count ----------")
+        print (f"Found {num_words} total words") 
+        print("--------- Character Count -------")    
+        for item in sorted_chars:
+              print(f"{item['char']}: {item['num']}")
+        print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -7,7 +7,18 @@ def count_characters(text):
             ch = ch.lower()
             counts[ch] = counts.get(ch, 0) +1
         return counts
-        
-def sort(text):
-       return counts.sort(reverse = true, key=sort)
+
+def sort_on(item):
+    return item["num"]
+
+def build_sorted_char_list(counts):
+        characters = []
+        for ch, n in counts.items():
+                if not ch.isalpha():
+                        continue
+                if not ("a" <= ch <= "z"):
+                        continue
+                characters.append({"char": ch, "num": n})
+        characters.sort(reverse=True, key=sort_on)
+        return characters
        
